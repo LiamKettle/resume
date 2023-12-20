e4e73cbe27928e84d9f062fc805fbf6ebf454043
LiamKettle <64558675+LiamKettle@users.noreply.github.com>
2023-12-20 15:29:03 -0500

    Updates

--- render_cv.r
+++ render_cv.r
@@ -18,4 +18,24 @@ rmarkdown::render("cv.rmd",
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "resume.pdf")
+                       output = "cv_test.pdf")
+
+
+
+
+### No sidebar
+# Knit the HTML version
+rmarkdown::render("cv-no-sidebar.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "cv-no-sidebar.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv-no-sidebar.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "cv-no-sidebar.pdf")
+
